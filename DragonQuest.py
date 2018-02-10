58de2a61fea4aab2a63a038ad2dd6bd0eaa2270b
shasegawa <anima0370@gmail.com>
2018-02-10 17:44:23 +0900

    関数だけ

--- DragonQuest.py
+++ DragonQuest.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from pages.api import BossList
+from pages.api import BossList, BossWeak
 
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def boss_list():
     return BossList.exec()
 
+
+@app.route('/api/GetBossWeak')
+def boss_list():
+    return BossWeak.exec()
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=8057, use_reloader=False)
---
+++ pages/api/BossWeak.py
@@ -0,0 +1,11 @@
+import json
+
+import libs.dao.dao as dao
+
+
+def exec():
+    return get_json()
+
+
+def get_json():
+    return '(๑´•.̫ • `๑)'
