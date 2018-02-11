2c4d4dc88502104e4efcba55960b8383ab17cbdd
shasegawa <anima0370@gmail.com>
2018-02-12 04:27:49 +0900

    弱点を返すAPI完成

--- pages/api/BossWeak.py
+++ pages/api/BossWeak.py
@@ -1,6 +1,7 @@
 import json
 
 import libs.dao.dao as dao
+from libs.dao.models import BossList, BossWeak
 
 
 def exec():
@@ -8,4 +9,19 @@ def exec():
 
 
 def get_json():
-    return '(๑´•.̫ • `๑)'
+    boss_model_list = dao.db_session.query(BossList).all()
+    response = {'list': []}
+    for boss_model in boss_model_list:
+        boss_weak = dao.db_session.query(BossWeak).filter(BossWeak.boss_list_id == boss_model.boss_list_id).all()[0]
+        weak = {'boss_list_id': boss_weak.boss_list_id,
+                'fire': boss_weak.fire,
+                'ice': boss_weak.fire,
+                'storm': boss_weak.fire,
+                'thunder': boss_weak.fire,
+                'soil': boss_weak.fire,
+                'dirk': boss_weak.fire,
+                'light': boss_weak.fire
+                }
+        response['list'].append(weak)
+
+    return json.dumps(response)
